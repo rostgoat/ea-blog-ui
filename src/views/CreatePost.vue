c12498d5e4cba277452bf0283e9c8c99ebc3d5e6
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-18 03:01:20 -0800

    removing boilerplate code; setting up vuetify; converting classes to es6; initial commit

---
+++ src/views/CreatePost.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Create Post</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class CreatePost extends Vue {}
+</script>
+
+<style></style>
