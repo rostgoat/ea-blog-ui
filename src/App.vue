87becbfcdea5697634549fdb9f5135d3e845d2d5
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-18 14:26:54 -0800

    added login and register pages; implementing router for both

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <v-app> </v-app>
+  <v-app>
+    <Login />
+    <Register />
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 
-@Component
+@Component({
+  components: {
+    Login,
+    Register
+  }
+})
 export default class App extends Vue {
   name = "App";
+  showPassword = false;
+  toggleRegister = false;
+  toggleLogin = true;
 }
 </script>
