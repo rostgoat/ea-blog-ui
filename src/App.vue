c8fbe007cc361c340d0e65a352557423cc518120
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-19 00:08:29 -0800

    setup toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <v-app>
-    <router-view />
+    <v-app-bar app dark>
+      <v-toolbar-title>EA Games Blog</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon @click="loginOrRegister">mdi-account-circle</v-icon>
+    </v-app-bar>
+    <v-content>
+      <router-view />
+    </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import Login from "@/views/Login.vue";
-import Register from "@/views/Register.vue";
 
-@Component({
-  components: {
-    Login,
-    Register
-  }
-})
+@Component
 export default class App extends Vue {
   name = "App";
-  showPassword = false;
-  toggleRegister = false;
-  toggleLogin = true;
+
+  loginOrRegister() {
+    this.$router.push("login");
+  }
 }
 </script>
