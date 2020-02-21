5569d2e3e9c09de4abaff04b61e02824f2567c19
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-20 20:11:38 -0800

    overriding vuetify styles

--- src/views/ListPosts.vue
+++ src/views/ListPosts.vue
@@ -1,20 +1,20 @@
 <template>
   <v-app>
-    <v-card class="mx-auto mt-5" outlined>
-      <v-container class="posts-list__content">
-        <v-card elevation="0" class="posts-list__img-container">
+    <v-card class="mx-auto mt-5 posts-list-item" outlined>
+      <v-container class="posts-list-item__content">
+        <v-card flat class="posts-list-item__img-container">
           <v-img
-            class="pa-2 posts-list__img"
+            class="pa-2 posts-list-item__img"
             src="https://picsum.photos/510/300?random"
             max-width="400"
             max-height="400"
           ></v-img>
         </v-card>
-        <v-card elevation="0">
-          <v-card-title class="mb-3 posts-list__title"
+        <v-card flat class="posts-list-item__description-container">
+          <v-card-title class="mb-4 posts-list-item__title"
             >Call of Duty: Black Ops 4</v-card-title
           >
-          <v-card-subtitle class="posts-list__subtitle">
+          <v-card-subtitle class="posts-list-item__subtitle">
             Integer vitae tellus ut quam convallis vestibulum. Duis tristique
             lacus augue, sed mollis turpis scelerisque nec. Donec imperdiet
             accumsan fermentum. Morbi quis metus congue diam aliquam commodo.
@@ -22,26 +22,25 @@
             felis blandit, nec tristique purus pulvinar.
           </v-card-subtitle>
 
-          <v-card-actions class="posts-list__actions">
-            <v-list-item class="grow ">
-              <v-list-item-content>
-                <v-list-item-title class="posts-list__actions-author"
-                  >Evan You</v-list-item-title
-                >
-              </v-list-item-content>
+          <v-card-actions class="posts-list-item__actions mt-3">
+            <div class="posts-list-item__actions-author">
+              Evan You
+            </div>
 
-              <v-row
-                align="center"
-                justify="end"
-                class="posts-list__actions-buttons"
-              >
-                <v-icon class="mr-1">mdi-heart</v-icon>
-                <span class="subheading mr-2">256</span>
-                <span class="mr-1">·</span>
-                <v-icon class="mr-1">mdi-share-variant</v-icon>
-                <span class="subheading">45</span>
-              </v-row>
-            </v-list-item>
+            <div class="posts-list-item__actions-date">
+              Feb 14, 2020
+            </div>
+
+            <v-row
+              align="center"
+              justify="end"
+              class="posts-list-item__actions-buttons"
+            >
+              <v-icon class="mr-1">mdi-heart</v-icon>
+              <span class="subheading mr-2">256</span>
+              <v-icon class="mr-1">mdi-share-variant</v-icon>
+              <span class="subheading">45</span>
+            </v-row>
           </v-card-actions>
         </v-card>
       </v-container>
@@ -60,15 +59,43 @@ export default class ListPosts extends Vue {
 </script>
 
 <style lang="scss">
-.posts-list {
-  &__title {
-  }
-
+.posts-list-item {
   &__content {
     display: flex;
   }
+
   &-img {
     background-position: 53% 53% !important;
+  }
+
+  &____img-container {
+    width: 33%;
+  }
+  &____description-container {
+    width: 66%;
+    display: flex;
+    flex-direction: column;
+    align-content: stretch;
+  }
+
+  &__title {
+    font-size: 2rem !important;
+  }
+
+  &__actions {
+    display: flex !important;
+    &-author {
+      flex: 1;
+    }
+
+    &-date {
+      flex: 1;
+      font-size: 1rem !important;
+    }
+
+    &-buttons {
+      flex: auto;
+    }
   }
 }
 </style>
