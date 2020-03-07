b77f806fd8b1213e4da1d265ee54f8c8451b4715
Rost Myshkin <rostmyshkin@gmail.com>
2020-03-06 16:12:24 -0800

    info logs

--- src/plugins/logger.ts
+++ src/plugins/logger.ts
@@ -32,7 +32,7 @@ const REQ_STYLE_STRING = `
   ${BASE_STYLES}
   background-image: linear-gradient(19deg, #EA7AF4 0%, #6200B3 100%);
   background-color: #FBDA61;
-  border-radius: 0 50px 50px 0;
+  border-radius: 0 0.0 0;
 `;
 
 const WARN_STYLE_STRING = `
@@ -59,31 +59,99 @@ const ERROR_PARAMS_STYLE_STRING = `
   border-radius: 0 50px 50px 0;
 `;
 
+const REQ_PARAMS_STYLE_STRING = `
+  ${BASE_STYLES}
+  color: white;
+  background-color: #FBDA61;
+  background-image: linear-gradient(19deg, rgba(117,185,129,1) 0%, rgba(148,175,0,1) 100%);
+  border-radius: 0 50px 50px 0;
+`;
+
 class NestLogger {
   private prefix = "";
+  private debug = false;
+
   constructor() {
     this.prefix = "EA API";
+    this.debug = process.env.VUE_APP_ENV !== "production";
   }
+
   public install(Vue: any) {
     Vue.prototype.$log = this;
   }
 
   error(text: string, response: any) {
-    console.log("text", text);
     const { status, statusText, config } = response;
     const paramsResponse = JSON.stringify(response, null, 1);
 
-    window.console.groupCollapsed(
-      `%c${this.prefix}%cError%c ${config.method.toUpperCase()} : ${
-        config.url
-      } : ${status} : ${statusText}`,
-      PREFIX_STYLE_STRING,
-      ERROR_STYLE_STRING,
-      ERROR_PARAMS_STYLE_STRING
-    );
-    window.console.error(paramsResponse);
-    window.console.groupEnd();
+    if (this.debug) {
+      window.console.groupCollapsed(
+        `%c${this.prefix}%cError%c ${config.method.toUpperCase()} : ${
+          config.url
+        } : ${status} : ${statusText}`,
+        PREFIX_STYLE_STRING,
+        ERROR_STYLE_STRING,
+        ERROR_PARAMS_STYLE_STRING
+      );
+      window.console.error(paramsResponse);
+      window.console.groupEnd();
+    }
     return text;
+  }
+
+  info(text: string) {
+    if (this.debug) {
+      window.console.info(
+        `%c${this.prefix}%cInfo%c ${text}`,
+        PREFIX_STYLE_STRING,
+        INFO_STYLE_STRING,
+        CLEAR_STYLE_STRING
+      );
+    }
+    return text;
+  }
+
+  warn(text: string) {
+    if (this.debug) {
+      window.console.group(
+        `%c${this.prefix}%cWarning%c`,
+        PREFIX_STYLE_STRING,
+        WARN_STYLE_STRING,
+        CLEAR_STYLE_STRING
+      );
+      window.console.warn(text);
+      window.console.groupEnd();
+    }
+    return text;
+  }
+
+  event(text: string) {
+    if (this.debug) {
+      window.console.info(
+        `%c${this.prefix}%cEvent%c ${text}`,
+        PREFIX_STYLE_STRING,
+        EVENT_STYLE_STRING,
+        CLEAR_STYLE_STRING
+      );
+    }
+    return text;
+  }
+
+  request(params: any) {
+    const paramsString = JSON.stringify(params, null, 1);
+    if (this.debug) {
+      window.console.groupCollapsed(
+        `%c${this.prefix}%cRequest%c ${params.method.toUpperCase()} : ${
+          params.url
+        }`,
+        PREFIX_STYLE_STRING,
+        REQ_STYLE_STRING,
+        REQ_PARAMS_STYLE_STRING
+      );
+      window.console.info(paramsString);
+      window.console.groupEnd();
+    }
+    return paramsString;
   }
 }
 
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,33 +1,38 @@
 import axios from "axios";
 import { Message, MessageBox } from "element-ui";
 import logger from "@/plugins/logger";
+import { UsersModule } from "@/store/modules/users";
+
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
   timeout: 5000
 });
 
-// // Request interceptors
-// service.interceptors.request.use(
-//   config => {
-//     // Add X-Access-Token header to every request, you can add other custom headers here
-//     console.log("UsersModule.token", UsersModule.token);
-//     if (UsersModule.token) {
-//       config.headers["X-Access-Token"] = UsersModule.token;
-//     }
-//     return config;
-//   },
-//   error => {
-//     Promise.reject(error);
-//   }
-// );
+// Request interceptors
+service.interceptors.request.use(
+  config => {
+    // Add X-Access-Token header to every request, you can add other custom headers here
+    if (UsersModule.token) {
+      config.headers["X-Access-Token"] = UsersModule.token;
+    }
+    logger.request({
+      method: config.method,
+      url: config.url
+    });
 
-// // Response interceptors
+    return config;
+  },
+  error => {
+    Promise.reject(error);
+  }
+);
+
+// Response interceptors
 service.interceptors.response.use(
   response => {
     return response.data;
   },
   error => {
-    console.log("error", { ...error });
     const { response } = error;
     logger.error(response.data.message, response);
     Message({
