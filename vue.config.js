094c8ae042731a818bd5bc3559edb00d4783b590
Rost Myshkin <rostmyshkin@gmail.com>
2020-03-23 16:45:51 -0700

    title and favicon changed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 module.exports = {
+  outputDir: path.resolve(__dirname, "../ea-blog-api/public"),
   devServer: {
     proxy: "http://localhost:3000"
   },
