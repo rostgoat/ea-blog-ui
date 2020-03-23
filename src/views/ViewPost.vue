e413ae56a5db0d96b6a77eb19ade904d241744b7
Rost Myshkin <rostmyshkin@gmail.com>
2020-03-22 22:32:31 -0700

    app logo; view post view

---
+++ src/views/ViewPost.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Post View</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class ViewPost extends Vue {
+  destinationId = this.$route.params.id;
+}
+</script>
+
+<style></style>
